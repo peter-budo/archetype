0c3ea2710c0fac8cd5c311dabe8d46cd56fb7163
Peter <peter.budo@gmail.com>
2012-12-10 12:36:34 +0000

    Initial commit after forking and removal of unwanted modules

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-archetype
-=========
+android-archetypes
+==================
+
+This projects provides several Maven archetypes for Android. Those archetypes allows to quickly bootstrap a Maven project
+to develop an android application.
+
+These artifacts are based on the android-maven-plugin (http://code.google.com/p/maven-android-plugin/). It currently uses the 3.4.0 version.
+
+The android-novoda archetype
+--------------------------------
+The archetype is based on experiences and setup used by [Novoda](http://www.novoda.com). This archetype will provided you with following modules:
+
+    app - main Android structure holder dependent on core
+    core - this is plain Java "zone", stuff that can be easier tested with JUnit and mocked with various frameworks (we try this section to be as much independent from Android, but sometimes some stuff sneak inside)
+    instrumentation  - integration tests using instrumentation, Robolectric, out-of-box setup for Emma reports you can use on your Jenkins CI etc
+
+To initiate an android project use:
+
+    mvn archetype:generate \
+      -DarchetypeArtifactId=android-novoda \
+      -DarchetypeGroupId=com.novoda \
+      -DarchetypeVersion=1.0.0-SNAPSHOT \
+      -DgroupId=your.company \
+      -DartifactId=my-android-application
+
+You can also set three optional properties :
+
+* The created 'package' with '-Dpackage=your.company.android'. By default it uses the given groupId.
+* The targeted Android platform with '-Dplatform=7'. The Android SDK version will be automatically fetched to fit the corresponding API level. Available API Level are 3, 4, 7, 8, 9, 10, 14 and 16. By default, it uses 16 (android 4.1.1.4).
+
+### Warning
+'-Demulator=my-avd' is not supported due different structure of archetype from above listed
+
+Once generated, the application is ready to be built and deployed (you may need to configure your ANDROID_HOME environment variable to point to your Android SDK). Start an android emulator, or plug an Android phone,
+and launch:
+
+    cd my-android-application
+    mvn clean install android:deploy
+
+The application will be built and deployed on the device.
+
+Setting the maven-android-plugin version
+----------------------------------------
+You can change the default maven-android-plugin version by specifying the 'android-plugin-version' parameter.
+
+Credits
+-------
+The android-archetype-project is an open source project licensed under the Apache License 2.0.
+It is founded by [akquinet](http://akquinet.de/en) and forked by [Novoda](http://novoda.com)
+
+Technical Notes
+---------------
+* As the archetypes used the new archetype format, they don't work with the deprecated goal 'archetype:create'
---
+++ android-archetypes-it/src/test/java/de/akquinet/android/archetypes/Helper.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2010 akquinet
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.akquinet.android.archetypes;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.Assert;
+
+public class Helper {
+    
+    public static void defineArchetypeVersion() {
+        if (System.getProperty("archetype.version") == null) {
+            System.setProperty("archetype.version", "1.0.9-SNAPSHOT");
+        }
+        System.out.println("archetype.version=" + System.getProperty("archetype.version"));
+    }
+
+    public static File prepareDirectory(String dir) {
+        File file = new File("target/it/" + dir);
+        if (file.exists()) {
+            delete(file);
+        }
+        file.mkdirs();
+        return file;
+    }
+
+    public static void delete(File file) {
+        if (file.exists()) {
+            if (file.isFile()) {
+                file.delete();
+            } else {
+                File[] files = file.listFiles();
+                for (File f : files) {
+                    delete(f);
+                }
+                file.delete();
+            }
+        }
+    }
+
+    public static final String readInputStream(InputStream is) throws IOException {
+        StringBuffer acc = new StringBuffer();
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = is.read(buf)) > 0) {
+          for (int i = 0; i < len; i++) {
+            acc.append((char) buf[i]);
+          }
+        }
+        is.close();
+        return acc.toString();
+
+    }
+
+    public static void assertContains(String txt, String fragment) {
+        if (txt.lastIndexOf(fragment) == -1) {
+            Assert.fail(" The text does not contain " + fragment);
+        }
+    }
+
+    public static void assertContains(File file, String fragment) throws IOException {
+        FileInputStream fis = new FileInputStream(file);
+        String out = readInputStream(fis);
+        fis.close();
+        if (out.lastIndexOf(fragment) == -1) {
+            Assert.fail(" The text does not contain " + fragment);
+        }
+    }
+
+    public static void assertNotContains(String txt, String fragment) {
+        if (txt.lastIndexOf(fragment) != -1) {
+            Assert.fail(" The text does contain " + fragment);
+        }
+    }
+
+
+
+
+}
